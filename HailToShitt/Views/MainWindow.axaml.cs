3643cb8fd81965db5d96600667359878d273789a
DiamondShadows <bsimon2005@gmail.com>
2023-10-18 16:20:07 -0400

    Continuing Game Logic

--- HailToShitt/Views/MainWindow.axaml.cs
+++ HailToShitt/Views/MainWindow.axaml.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Threading;
 using System;
+using System.Drawing;
+using System.Transactions;
 using Avalonia.Controls;
 
 namespace HailToShit.Views;
@@ -12,7 +14,7 @@ public partial class MainWindow : Window
     {
         gameThread = new Thread(new ThreadStart(GameLoop));
         InitializeComponent();
-        
+        Graphics g;
         gameThread.Start();
         
     }
@@ -24,8 +26,20 @@ public partial class MainWindow : Window
             Console.WriteLine("Game Looop Running");
             Thread.Sleep(100);
             //TODO: Update information such as character position
+            update();
             //TODO: Draw the screen with the updated information
+            
         }
             
+    }
+
+    public void update()
+    {
+        
+    }
+
+    public void paintComponent(Graphics g)
+    {
+        paintComponent(g);
     }
 }
