474ea6752f7ce9956ca0cb3acf7aaf02f0bfdeda
DiamondShadows <bsimon2005@gmail.com>
2023-10-18 15:12:43 -0400

    Game Loop Logic Developed

--- HailToShitt/Views/MainWindow.axaml.cs
+++ HailToShitt/Views/MainWindow.axaml.cs
@@ -1,25 +1,31 @@
 using System;
 using System.Threading;
+using System;
 using Avalonia.Controls;
 
 namespace HailToShit.Views;
 
 public partial class MainWindow : Window
 {
+    Thread gameThread;
     public MainWindow()
     {
+        gameThread = new Thread(new ThreadStart(GameLoop));
         InitializeComponent();
-        Thread thread = new Thread(new ThreadStart(GameLoop));
-        thread.Start();
+        
+        gameThread.Start();
         
     }
     
-
-    private void GameLoop()
+    public void GameLoop()
     {
-            
-            Console.WriteLine("Hello");
+        while (gameThread != null)
+        {
+            Console.WriteLine("Game Looop Running");
             Thread.Sleep(100);
+            //TODO: Update information such as character position
+            //TODO: Draw the screen with the updated information
+        }
             
     }
 }
